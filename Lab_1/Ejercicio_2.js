bdd1a0a096ed955f719760217f00d5ffeb1179f9
DaddyHank <elmago2301@gmail.com>
2024-01-28 12:01:13 -0600

    deleted

--- Lab_1/Ejercicio_2.js
+++
@@ -1,6 +0,0 @@
-//nombre y carnet
-let nombre = 'Jose Alcides Granados Escobar';
-let carnet = 'SMSS132722';
-
-//concatenacion
-console.log(nombre+','+carnet);
--- Lab_1/Ejercicio_3.js
+++
@@ -1,8 +0,0 @@
-//numero para usar
-let numero = 24;
-
-//numero a string
-let string = numero.toString();
-
-//impresion
-console.log(string);
