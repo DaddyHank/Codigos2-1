426bd8fc27cafef11d934ceb1b5add367068e752
DaddyHank <elmago2301@gmail.com>
2024-01-30 07:40:30 -0600

    update

---
+++ practica_3/scripts.js
@@ -0,0 +1,8 @@
+function sumar(num1,num2){
+    //console.log(num1+num2);
+    //document.write(num1+num2);
+    var n1 = Number((document.getElementById("num1")).value);
+    var n2 = Number((document.getElementById("num2")).value);
+    var suma = n1+n2;
+    document.write(suma);
+}
